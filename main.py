16caa62a53e177198110c8dd32c81bf56abf28e0
sydbermas <sydbermas@gmail.com>
2021-08-24 12:16:02 +0800

    template-match-only

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
-
 import cv2 as cv
 import numpy as np
+import glob
 from matplotlib import pyplot as plt
 
-#if placed{} 
-img = cv.imread('test1.jpg',0)
-img2 = img.copy()
+#empty list to store template images
+template_data=[]
+#make a list of all template images from a directory
+files1= glob.glob('templates\\*.png')
 
-#const
-template = cv.imread('templates/1.png',0)
+for myfile in files1:
+    image = cv.imread(myfile,0)
+    template_data.append(image)
 
-w, h = template.shape[::-1]
-# All the 6 methods for comparison in a list
+test_image=cv.imread('original.png')    # acting camera
+test_image= cv.cvtColor(test_image, cv.COLOR_BGR2GRAY)
 methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
-# Apply template Matching
-    res = cv.matchTemplate(img,template,method)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+#loop for matching
+for tmp in template_data:
+    (tH, tW) = tmp.shape[:2]
+    cv.imshow("Template", tmp)
+    cv.waitKey(1000)
+    cv.destroyAllWindows()
+    for meth in methods:
+        method = eval(meth)
+    
+    result = cv.matchTemplate(test_image, tmp, method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+
     if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
         top_left = min_loc
     else:
         top_left = max_loc
-bottom_right = (top_left[0] + w, top_left[1] + h)
-cv.rectangle(img,top_left, bottom_right, 255, 2)
-plt.subplot(121),plt.imshow(res,cmap = 'gray')
-plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(img,cmap = 'gray')
-plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-plt.suptitle(meth)
+
+    bottom_right = (top_left[0] + tW, top_left[1] + tH)
+    cv.rectangle(test_image,top_left, bottom_right,255, 2)
+    plt.subplot(121),plt.imshow(result,cmap = 'gray')
+    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(test_image,cmap = 'gray')
+    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    plt.suptitle(meth)
 plt.show()
