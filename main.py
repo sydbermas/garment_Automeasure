272ee295a420cc906d32266b9fd2a068f46842de
sydbermas <sydbermas@gmail.com>
2021-08-25 08:22:55 +0800

    chest-pom-ab

--- main.py
+++ main.py
@@ -1,43 +1,54 @@
 import cv2 as cv
 import numpy as np
-import glob
 from matplotlib import pyplot as plt
+import glob
 
-#empty list to store template images
-template_data=[]
-#make a list of all template images from a directory
-files1= glob.glob('templates\\*.png')
+imageToBeInspected = 'styleNum.jpeg'    #acting as camera
+# poms = [['cropped3.jpeg','cropped4.jpeg']]
+poms = [glob.glob('styleNum\\*.jpeg')]
+pomOffset =[[157,393,213,350]]
+pomIndex = 0
+pixelToInch = 57.37
+result = []
 
-for myfile in files1:
-    image = cv.imread(myfile,0)
-    template_data.append(image)
+for pom in poms:
 
-test_image=cv.imread('original.png')    # acting camera
-test_image= cv.cvtColor(test_image, cv.COLOR_BGR2GRAY)
-methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
-#loop for matching
-for tmp in template_data:
-    (tH, tW) = tmp.shape[:2]
-    cv.imshow("Template", tmp)
-    cv.waitKey(1000)
-    cv.destroyAllWindows()
-    for meth in methods:
-        method = eval(meth)
-    
-    result = cv.matchTemplate(test_image, tmp, method)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-
+    img = cv.imread(imageToBeInspected,0)
+    img2 = img.copy()
+    template = cv.imread(pom[0],0)
+    template2 = cv.imread(pom[1],0)
+    w, h = template.shape[::-1]
+    w2, h2 = template2.shape[::-1]
+    # All the 6 methods for comparison in a list
+    methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
+            'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+    meth = methods[1] # Mehtod setting
+    img = img2.copy()
+    method = eval(meth)
+    # Apply template Matching
+    res = cv.matchTemplate(img,template,method)
+    res2 = cv.matchTemplate(img,template2,method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    min_val2, max_val2, min_loc2, max_loc2 = cv.minMaxLoc(res2)
+    # print(min_val, max_val, min_loc, max_loc)
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
     if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
         top_left = min_loc
+        top_left2 = min_loc2
     else:
         top_left = max_loc
+        top_left2 = max_loc2
+    pomA = (top_left[0] + pomOffset[pomIndex][0], top_left[1] + pomOffset[pomIndex][1])
+    pomB = (top_left2[0] + pomOffset[pomIndex][2], top_left2[1] + pomOffset[pomIndex][3])
+    print(pomA,pomB)
+    result.append((pomB[0]-pomA[0])/pixelToInch)
+    print(round(result[pomIndex],3))
 
-    bottom_right = (top_left[0] + tW, top_left[1] + tH)
-    cv.rectangle(test_image,top_left, bottom_right,255, 2)
-    plt.subplot(121),plt.imshow(result,cmap = 'gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(test_image,cmap = 'gray')
+    cv.rectangle(img,pomA, pomB, 255, 3)
+    # plt.subplot(121),plt.imshow(res,cmap = 'gray')
+    # plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    plt.subplot(121),plt.imshow(img,cmap = 'gray')
     plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
     plt.suptitle(meth)
-plt.show()
+    plt.show()
+    pomIndex +=1
---
+++ test2.py
@@ -0,0 +1,49 @@
+import cv2 as cv
+import numpy as np
+import glob
+from matplotlib import pyplot as plt
+from string import ascii_uppercase
+#empty list to store template images
+template_data=[]
+#make a list of all template images from a directory
+files1= glob.glob('templates\\*.png')
+
+for myfile in files1:
+    image = cv.imread(myfile,0)
+    template_data.append(image)
+
+test_image=cv.imread('original.png')    # acting camera
+test_image= cv.cvtColor(test_image, cv.COLOR_BGR2GRAY)
+methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
+'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+
+#loop for matching
+for tmp in template_data:
+    (tH, tW) = tmp.shape[:2]
+    
+    cv.imshow("Template", tmp)
+    cv.waitKey(1000)
+    cv.destroyAllWindows()
+    for meth in methods:
+        method = eval(meth)
+    
+    result = cv.matchTemplate(test_image, tmp, method)
+  
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+
+    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+        top_left = min_loc
+    else:
+        top_left = max_loc
+        
+
+    bottom_right = (top_left[0] + tW, top_left[1] + tH)
+    # cv.rectangle(test_image,top_left, bottom_right,255, 2)
+    cv.circle(test_image,top_left, 20 ,255, 10)
+    plt.subplot(121),plt.imshow(result,cmap = 'gray')
+    print(result.shape)
+    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(test_image,cmap = 'gray')
+    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    # plt.suptitle(meth)
+plt.show()
