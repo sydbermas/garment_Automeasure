4f8462c78915d7896450671e05c07ec26ff30820
sydbermas <sydbermas@gmail.com>
2021-09-04 16:16:53 +0800

    added-grid

--- sourcecodes/camera-button.py
+++ sourcecodes/camera-button.py
@@ -20,11 +20,12 @@ def prompt_ok(event = 0):
     # button1.place(anchor=tk.CENTER, relx=0.2, rely=0.9, width=150, height=50)
     button2.place(anchor=tk.CENTER, relx=0.8, rely=0.9, width=150, height=50)
     # button1.focus()
+   
     saveAndExit()
     pom()
+    resume()
 
-def def_pos(event = 0):
-    
+
 
 def saveAndExit(event = 0):
     global prevImg
@@ -83,7 +84,6 @@ cap = cv2.VideoCapture(camIndex,cv2.CAP_DSHOW)
 capWidth = cap.get(3)
 capHeight = cap.get(4)
 
-
 success, frame = cap.read()
 if not success:
     if camIndex == 0:
@@ -102,7 +102,7 @@ mainWindow.resizable(width=False, height=False)
 mainWindow.bind('<Escape>', lambda e: mainWindow.quit())
 lmain = tk.Label(mainWindow, compound=tk.CENTER, anchor=tk.CENTER, relief=tk.RAISED)
 button = tk.Button(mainWindow, text="Capture", command=prompt_ok)
-button1 = tk.Button(mainWindow,text="Set Position", command=def_pos)
+
 
 
 button_changeCam = tk.Button(mainWindow, text="Switch Camera", command=changeCam)
@@ -110,8 +110,7 @@ button_changeCam = tk.Button(mainWindow, text="Switch Camera", command=changeCam
 lmain.pack()
 button.place(bordermode=tk.INSIDE, relx=0.5, rely=0.9, anchor=tk.CENTER, width=300, height=50)
 button.focus()
-button1.place(bordermode=tk.INSIDE, relx=0.5, rely=0.9, anchor=tk.CENTER, width=300, height=50)
-button1.focus()
+
 
 button_changeCam.place(bordermode=tk.INSIDE, relx=0.85, rely=0.1, anchor=tk.CENTER, width=150, height=50)
 
@@ -119,7 +118,29 @@ def show_frame():
     global cancel, prevImg, button
 
     _, frame = cap.read()
-    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Read logo and resize
+    logo = cv2.imread('contours.png')
+    # size = 400
+    logo = cv2.resize(logo, (640, 480))
+
+
+    cv2image = cv2.cvtColor(LOGO, cv2.COLOR_BGR2RGB)
+    ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
+
+    while cap.isOpened():
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret:
+            # Flip the frame
+            frame = cv2.flip(frame, 1)
+
+            # Region of Image (ROI), where we want to insert logo
+            roi = frame
+
+            # Set an index of where the mask is
+            roi[np.where(mask)] = 0
+            roi += logo
 
     prevImg = Image.fromarray(cv2image)
     imgtk = ImageTk.PhotoImage(image=prevImg)
@@ -128,33 +149,6 @@ def show_frame():
     if not cancel:
         lmain.after(10, show_frame)
 
-# show_frame()
+show_frame()
 
-def show_pos():
-    global cancel, prevImg, button1
-    logo = cv2.imread('contours.png')
-    # size = 400
-    logo = cv2.resize(logo, (640, 480))
-    img2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
-    ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
-    ret, frame = cap.read()
-    if ret:
-        # Flip the frame
-        frame = cv2.flip(frame, 1)
-
-        # Region of Image (ROI), where we want to insert logo
-        roi = frame
-
-        # Set an index of where the mask is
-        roi[np.where(mask)] = 0
-        roi += logo
-
-    prevImg = Image.fromarray(frame)
-    imgtk = ImageTk.PhotoImage(image=prevImg)
-    lmain.imgtk = imgtk
-    lmain.configure(image=imgtk)
-    if not cancel:
-        lmain.after(10, show_pos)
-
-show_pos()
 mainWindow.mainloop()
