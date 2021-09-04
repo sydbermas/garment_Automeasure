907bcd63b66affa8a9c9c2761df662f60a479341
sydbermas <sydbermas@gmail.com>
2021-09-04 13:48:23 +0800

    remove-grid

--- sourcecodes/camera-button.py
+++ sourcecodes/camera-button.py
@@ -15,16 +15,15 @@ def prompt_ok(event = 0):
 
     button.place_forget()
    
-    # button1 = tk.Button(mainWindow, text="Good Image!", command=saveAndExit)
+   
     button2 = tk.Button(mainWindow, text="Try Again", command=resume)
-    # button1.place(anchor=tk.CENTER, relx=0.2, rely=0.9, width=150, height=50)
+   
     button2.place(anchor=tk.CENTER, relx=0.8, rely=0.9, width=150, height=50)
-    # button1.focus()
+   
     saveAndExit()
     pom()
 
-def def_pos(event = 0):
-    
+
 
 def saveAndExit(event = 0):
     global prevImg
@@ -44,7 +43,7 @@ def resume(event = 0):
 
     cancel = False
 
-    # button1.place_forget()
+  
     button2.place_forget()
 
     mainWindow.bind('<Return>', prompt_ok)
@@ -102,7 +101,7 @@ mainWindow.resizable(width=False, height=False)
 mainWindow.bind('<Escape>', lambda e: mainWindow.quit())
 lmain = tk.Label(mainWindow, compound=tk.CENTER, anchor=tk.CENTER, relief=tk.RAISED)
 button = tk.Button(mainWindow, text="Capture", command=prompt_ok)
-button1 = tk.Button(mainWindow,text="Set Position", command=def_pos)
+
 
 
 button_changeCam = tk.Button(mainWindow, text="Switch Camera", command=changeCam)
@@ -110,8 +109,7 @@ button_changeCam = tk.Button(mainWindow, text="Switch Camera", command=changeCam
 lmain.pack()
 button.place(bordermode=tk.INSIDE, relx=0.5, rely=0.9, anchor=tk.CENTER, width=300, height=50)
 button.focus()
-button1.place(bordermode=tk.INSIDE, relx=0.5, rely=0.9, anchor=tk.CENTER, width=300, height=50)
-button1.focus()
+
 
 button_changeCam.place(bordermode=tk.INSIDE, relx=0.85, rely=0.1, anchor=tk.CENTER, width=150, height=50)
 
@@ -128,33 +126,6 @@ def show_frame():
     if not cancel:
         lmain.after(10, show_frame)
 
-# show_frame()
-
-def show_pos():
-    global cancel, prevImg, button1
-    logo = cv2.imread('contours.png')
-    # size = 400
-    logo = cv2.resize(logo, (640, 480))
-    img2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
-    ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
-    ret, frame = cap.read()
-    if ret:
-        # Flip the frame
-        frame = cv2.flip(frame, 1)
-
-        # Region of Image (ROI), where we want to insert logo
-        roi = frame
-
-        # Set an index of where the mask is
-        roi[np.where(mask)] = 0
-        roi += logo
-
-    prevImg = Image.fromarray(frame)
-    imgtk = ImageTk.PhotoImage(image=prevImg)
-    lmain.imgtk = imgtk
-    lmain.configure(image=imgtk)
-    if not cancel:
-        lmain.after(10, show_pos)
-
-show_pos()
+show_frame()
 mainWindow.mainloop()
+
